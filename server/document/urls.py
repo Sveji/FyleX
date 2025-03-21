cc81fc3e8cfc14ff5cd9f6bc428f6751a113d49c
anastassow <127148449+anastassow@users.noreply.github.com>
2025-03-21 21:22:09 +0200

    COSUMERS AND URLS ready

--- server/document/urls.py
+++ server/document/urls.py
@@ -3,7 +3,6 @@ from .views import document, get_summary, get_review
 
 urlpatterns = [
     path('', document, name = "upload_document"),
-    #path('<int:id>/', document, name = "delete_document"),
     path('summary/', get_summary, name = "get_summary"),
     path('review/', get_review, name = "get_review"),
 ]
