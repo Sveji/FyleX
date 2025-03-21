4814e447165d29217556f17eda72e6c0cf3df2d8
anastassow <127148449+anastassow@users.noreply.github.com>
2025-03-22 00:53:15 +0200

    fix url

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register, login, user, activate_user, forgot_password, reset_password
+from .views import register, login, user, activate_user, forgot_password, reset_password, google_login
 
 urlpatterns = [
     path('register/', register, name='register'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', activate_user, name='activate-user'),
     path('forgot-password/', forgot_password, name='forgot_password'),
     path('reset-password/<uidb64>/<token>/', reset_password, name='reset_password'),
+    path('google-login/', google_login),
 ]
