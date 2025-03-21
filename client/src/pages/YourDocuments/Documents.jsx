53c1af1c758d9c236b743dad6742a47958a91cfa
MrTiNnN <martin.v.velchev@gmail.com>
2025-03-21 15:11:33 +0200

    My documents

--- client/src/pages/YourDocuments/Documents.jsx
+++ client/src/pages/YourDocuments/Documents.jsx
@@ -1,18 +1,23 @@
 import { LuFileText } from "react-icons/lu";
 
 import './yourdocuments.less'
+import { useContext } from "react";
+import { DataContext } from "../../context/DataContext";
 
 
-const Documents = () => {
+const Documents = ({ document }) => {
+    // Gets global data from the context
+    const { navigate } = useContext(DataContext)
+
 
 
     return (
         <>
-            <div className="file-container">
+            <div className="file-container" onClick={() => navigate(`/document/${document.id}`)}>
                 <div className="file">
-                    <LuFileText size={32} color={"#000000"} fill={"#7E4F83"} />
+                    <LuFileText className="icon" size={32} color={"#000000"} fill={"#7E4F83"} />
                     <div className="file-title">
-                        <p>MikiEPrasence.pdf</p>
+                        <p className="title">MikiEPrasence.pdf</p>
                         <div classname="p1" >32 TB</div>
                     </div>
 
