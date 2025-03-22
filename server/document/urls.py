2dff064e7d7748b60f887c62cf0afb16d18b0921
anastassow <127148449+anastassow@users.noreply.github.com>
2025-03-22 06:09:15 +0200

    made the review in the create post veiw

--- server/document/urls.py
+++ server/document/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import document, get_summary, get_review
+from .views import document, get_summary
 
 urlpatterns = [
     path('', document, name = "upload_document"),
     path('summary/', get_summary, name = "get_summary"),
-    path('review/', get_review, name = "get_review"),
 ]
